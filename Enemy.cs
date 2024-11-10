c2da242b3cd8613937f13aa333085c61440b0bd8
brian-44 <144145680+brian-44@users.noreply.github.com>
2024-11-10 16:26:15 -0500

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public GameObject explosion;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        
+    }
 
-        if (transform.position.y < -8f)
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if (whatDidIHit.tag == "Player")
         {
+            GameObject.Find("Player(Clone)").GetComponent<Player>().LoseALife();
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        } else if (whatDidIHit.tag == "Weapon")
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(5);
+            Destroy(whatDidIHit.gameObject);
+            Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
--- Player.cs
+++ Player.cs
@@ -1,49 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //variables
-    //1. access level: public or private
-    //2. type: int (e.g., 2, 4, 123, 3456, etc.), float (e.g, 2.5, 3.67, etc.)
-    //3. name: (1) start w/ lowercase (2) if it is multiple words, then the other words start with uppercase and written together
-    //4. optional: give it an initial value
 
     private float horizontalInput;
     private float verticalInput;
     private float speed;
-    private int lives;
+    private float horizontalScreenLimit;
+    private float verticalScreenLimit;
 
+    public GameObject explosion;
     public GameObject bullet;
+    public int lives;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 6f;
-        lives = 3;
+        horizontalScreenLimit = 11.5f;
+        verticalScreenLimit = 3.5f;
+        lives = 3; 
+        FindObjectOfType<GameManager>().SetLivesCount(lives);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Moving();
+        Movement();
         Shooting();
     }
 
-    void Moving()
+    void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
-
-        //if my x position is bigger than 11.5f than I am outside the screen from the right 
-        if (transform.position.x > 11.5f || transform.position.x <= -11.5f)
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-
-        if (transform.position.y > 1f || transform.position.y <= -4f)
+        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
@@ -51,12 +50,23 @@ public class Player : MonoBehaviour
 
     void Shooting()
     {
-        //if SPACE key is pressed create a bullet; what is a bullet?
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            //create a bullet object at my position with my rotation
-            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity); 
+            Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
 
+    public void LoseALife()
+    {
+        //lives = lives - 1;
+        //lives -= 1;
+        lives--;
+        FindObjectOfType<GameManager>().loselives(lives);
+        if (lives == 0)
+            
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
 }
